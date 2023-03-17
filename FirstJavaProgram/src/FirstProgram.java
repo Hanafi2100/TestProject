bc50bdab4df5d9dab91cc904bb03b81626a66c8c
User <User@10.6.124.83>
2023-03-17 17:32:14 +0800

    third

---
+++ FirstJavaProgram/src/FirstProgram.java
@@ -0,0 +1,10 @@
+
+public class FirstProgram {
+	public static void main(String[] args)
+	{
+		System.out.print("Hello! ");
+		System.out.print("Java");
+		System.out.print("Problem Driven");
+	}
+
+}
